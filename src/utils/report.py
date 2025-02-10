a35dc89887236c5a0229d7e82d75a3a9fdcbd43c
cvantienen <codyvantienen@gmail.com>
2025-02-10 17:12:23 -0500

    Analysis Results Dic Converted into Attributes

--- src/utils/report.py
+++ src/utils/report.py
@@ -2,21 +2,37 @@ import os
 import polars as pl
 from datetime import date
 from docxtpl import DocxTemplate
+import subprocess
 from test.sampleCompany import get_sample_company
 
 # TODO: Add more analysis methods, and styling to the report
 class SamplePriceComp:
     def __init__(self, conn, contract_number):
         self.conn = conn
-        self.company =  get_sample_company(contract_number)
+        self.company = get_sample_company(contract_number)
         self.df = None
         self.manufacture_analysis_df = None
         self.analysis_results = None
-        self.output_path = "/app/output/"     # Output directory
+        self.output_path = "/app/output/"
+        
+        # Analysis results as attributes
+        self.product_count = None
+        self.below_competitor = None
+        self.above_competitor = None
+        self.avg_percent_diff = None
+        self.max_percent_diff = None
+        self.min_percent_diff = None
+        self.avg_price_deviation = None
+        self.max_price_deviation = None
+        self.min_price_deviation = None
+        self.deviate_more_than_1 = None
+        self.deviate_more_than_10 = None
+        self.deviate_more_than_100 = None
 
 
     def get_100_random_products(self):
-        """ Get 100 random items from the specific contract from the database and load it into a DataFrame."""
+        """ Get 100 random items from the specific contract
+            from the database and load it into a DataFrame."""
         query = f"""
         WITH reference_items AS (
             SELECT *
@@ -43,8 +59,10 @@ class SamplePriceComp:
         self.df = pl.read_database(query, self.conn)
         self.df = self.df.with_columns(pl.col("price").cast(pl.Float64))
 
+        print(f"Loaded {self.product_count} products for analysis.")
+
     def analyze_data(self):
-        """Analyze the DataFrame and store the results."""
+        """Analyze the Sample DataFrame and store the results."""
         df = self.df
 
         # Calculate the comp average price for each manufacturer_part_number
@@ -80,21 +98,21 @@ class SamplePriceComp:
         )
 
         # Calculate general statements based on the Price % difference From GSA Average
-        below_competitor = combined_df.filter(pl.col("percent_difference") < 0).shape[0]
-        above_competitor = combined_df.filter(pl.col("percent_difference") > 0).shape[0]
-        avg_percent_diff = combined_df.select(pl.col("percent_difference").mean()).item()
-        max_percent_diff = combined_df.select(pl.col("percent_difference").max()).item()
-        min_percent_diff = combined_df.select(pl.col("percent_difference").min()).item()
+        self.below_competitor = combined_df.filter(pl.col("percent_difference") < 0).shape[0]
+        self.above_competitor = combined_df.filter(pl.col("percent_difference") > 0).shape[0]
+        self.avg_percent_diff = combined_df.select(pl.col("percent_difference").mean()).item()
+        self.max_percent_diff = combined_df.select(pl.col("percent_difference").max()).item()
+        self.min_percent_diff = combined_df.select(pl.col("percent_difference").min()).item()
 
         # Calculate Deviation
-        avg_price_deviation = combined_df.select(pl.col("price_deviation").mean()).item()
-        max_price_deviation = combined_df.select(pl.col("price_deviation").max()).item()
-        min_price_deviation = combined_df.select(pl.col("price_deviation").min()).item()
+        self.avg_price_deviation = combined_df.select(pl.col("price_deviation").mean()).item()
+        self.max_price_deviation = combined_df.select(pl.col("price_deviation").max()).item()
+        self.min_price_deviation = combined_df.select(pl.col("price_deviation").min()).item()
 
         # Count items that price deviation is more than $1, $10, and $100
-        count_more_than_1 = combined_df.filter(pl.col("price_deviation") > 1).shape[0]
-        count_more_than_10 = combined_df.filter(pl.col("price_deviation") > 10).shape[0]
-        count_more_than_100 = combined_df.filter(pl.col("price_deviation") > 100).shape[0]
+        self.deviate_more_than_1 = combined_df.filter(pl.col("price_deviation") > 1).shape[0]
+        self.deviate_more_than_10 = combined_df.filter(pl.col("price_deviation") > 10).shape[0]
+        self.deviate_more_than_100 = combined_df.filter(pl.col("price_deviation") > 100).shape[0]
 
         # Group by manufacturer_name and calculate the average percent difference
         manufacturer_percent_diff = combined_df.group_by("manufacturer_name").agg(
@@ -110,24 +128,13 @@ class SamplePriceComp:
 
         formatted_percent_manufacture_diff = manufacturer_percent_diff.with_columns(
             pl.col("average_percent_difference").abs().map_elements(
-                lambda x: f"{x * 100:.2f}%" if x is not None else "N/A")
+                lambda x: f"{x * 100:.2f}%" if x is not None else "N/A",
+                return_dtype=pl.Utf8
+            )
         )
 
         self.manufacture_analysis_df = formatted_percent_manufacture_diff
 
-        self.analysis_results = {
-            "below_competitor": below_competitor,
-            "above_competitor": above_competitor,
-            "avg_percent_diff": avg_percent_diff,
-            "max_percent_diff": max_percent_diff,
-            "min_percent_diff": min_percent_diff,
-            "avg_price_deviation": avg_price_deviation,
-            "max_price_deviation": max_price_deviation,
-            "min_price_deviation": min_price_deviation,
-            "count_more_than_1": count_more_than_1,
-            "count_more_than_10": count_more_than_10,
-            "count_more_than_100": count_more_than_100,
-        }
 
     def generate_report(self):
         """Generate a Word document report based on the analysis results using a template."""
@@ -140,35 +147,72 @@ class SamplePriceComp:
             'company_name': self.company,
             'current_date': date.today(),
             'contract_number': self.company.contract_number,
-"""         'company_uie': self.company.sam_uei,
-            'current_option_period_start_date': self.company.current_option_period_start_date,
-            'current_option_period_end_date': self.company.current_option_period_end_date,
-            'ultimate_end_date': self.company.ultimate_contract_end_date,"""
+            'sam_uie': self.company.sam_uei,
             'option_end_date': self.company.current_option_period_end_date,
             'ultimate_end_date': self.company.ultimate_contract_end_date,
-            'below_competitor': self.analysis_results['below_competitor'],
-            'above_competitor': self.analysis_results['above_competitor'],
-            'avg_percent_diff': f"{self.analysis_results['avg_percent_diff']:.2f}%",
-            'max_percent_diff': f"{self.analysis_results['max_percent_diff']:.2f}%",
-            'min_percent_diff': f"{self.analysis_results['min_percent_diff']:.2f}%",
-            'avg_price_deviation': f"${self.analysis_results['avg_price_deviation']:.2f}",
-            'max_price_deviation': f"${self.analysis_results['max_price_deviation']:.2f}",
-            'min_price_deviation': f"${self.analysis_results['min_price_deviation']:.2f}",
-            'count_more_than_1': self.analysis_results['count_more_than_1'],
-            'count_more_than_10': self.analysis_results['count_more_than_10'],
-            'count_more_than_100': self.analysis_results['count_more_than_100'],
+            'product_count': self.product_count,
+            'below_competitor': self.below_competitor,
+            'above_competitor': self.above_competitor,
+            'avg_percent_diff': f"{self.avg_percent_diff:.2f}%",
+            'max_percent_diff': f"{self.max_percent_diff:.2f}%",
+            'min_percent_diff': f"{self.min_percent_diff:.2f}%",
+            'avg_price_deviation': f"${self.avg_price_deviation:.2f}",
+            'max_price_deviation': f"${self.max_price_deviation:.2f}",
+            'min_price_deviation': f"${self.min_price_deviation:.2f}",
+            'deviate_more_than_1': self.deviate_more_than_1,
+            'deviate_more_than_10': self.deviate_more_than_10,
+            'deviate_more_than_100': self.deviate_more_than_100,
             'manufacture_analysis_df': self.manufacture_analysis_df.to_dicts()  # Convert DataFrame to list of dicts
         }
 
-        # For testing
-        print(context['manufacture_analysis_df'])
         # Render the template with the context
         doc.render(context)
 
-        # Save the rendered document
-        file_name = f"SampleReport_{self.company.contract_number}.docx"
-        full_path = os.path.join(self.output_path, file_name)
-        doc.save(full_path)
+        # Save the rendered Word document
+        file_name = f"GSA_Report_{self.company.contract_number}.docx"
+        word_file_path = os.path.join(self.output_path, file_name)
+        doc.save(word_file_path)
+
+        # Convert the Word document to PDF
+        pdf_file_path = self.docx_to_pdf(word_file_path)
+
+        # remove the Word file
+        os.remove(word_file_path)
+
+        return pdf_file_path
+
+    def docx_to_pdf(self, word_file_path):
+        subprocess.call(['soffice',
+                         '--headless',
+                         '--convert-to',
+                         'pdf',
+                         '--outdir',
+                         self.output_path,
+                         word_file_path])
+
+        # Get the base name of the input document (without extension)
+        base_name = os.path.splitext(os.path.basename(word_file_path))[0]
+
+        # Create the output PDF file path
+        pdf_path = os.path.join(self.output_path, base_name + '.pdf')
+
+        # Return the PDF file path
+        return pdf_path
+
+    def run_sample_report(self):
+        self.get_100_random_products()
+        self.analyze_data()
+        self.generate_report()
+
+"""    def docx_to_pdf(self, word_file_path):
+
+        # Convert the Word document file path to the PDF file path
+        pdf_file_name = f"GSA_Report_{self.company.contract_number}.pdf"
+        pdf_file_path = os.path.join(self.output_path, pdf_file_name)
+
+        return pdf_file_path
+"""
+
 
 """
         # Add the table after rendering
